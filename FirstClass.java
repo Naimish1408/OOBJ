eb4498dafb014d0cba5ced4da1c9ccb7837c32e2
Naimish1408 <ahirnaimish9696@gmail.com>
2024-12-31 19:38:12 +0530

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class FirstClass{
+    public static void main(String args[])
+    {
+        int a;
+        System.out.println("Enter Number");
+        Scanner S=new Scanner(System.in);
+        a=S.nextInt();
+        System.out.println(a);
+    }
+}
